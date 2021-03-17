5698d983ee9100933cba459d7d651dd501c18f31
osintsevse <osintsevse@gmail.com>
2021-03-18 00:10:29 +0300

    added view spring boot  config for jpa

---
+++ view/src/main/java/com/thirdwheel/carbase/view/configuration/JpaConfig.java
@@ -0,0 +1,40 @@
+package com.thirdwheel.carbase.view.configuration;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.Properties;
+
+@Configuration
+@ConfigurationProperties(prefix = "jpa")
+@EnableTransactionManagement
+public class JpaConfig {
+    private Properties properties = new Properties();
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setPersistenceUnitName("huy-persistence-unit");
+        emf.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        emf.setPackagesToScan("com.stas.learn.dao");
+        emf.setJpaProperties(getProperties());
+
+        return emf;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+}
