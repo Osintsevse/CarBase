ba5bad1a37cba5a47dfdf6ae59952b1784f00deb
osintsevse <osintsevse@gmail.com>
2021-03-27 17:54:49 +0300

    fixed Launcher

--- view/src/main/java/com/thirdwheel/carbase/view/Launcher.java
+++ view/src/main/java/com/thirdwheel/carbase/view/Launcher.java
@@ -1,11 +1,12 @@
 package com.thirdwheel.carbase.view;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
 public class Launcher {
     public static void main(String[] args) {
-
+        SpringApplication.run(Launcher.class);
     }
 }
