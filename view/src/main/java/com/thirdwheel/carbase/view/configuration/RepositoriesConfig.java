486e2cd90a59f9be5391af387f05c05de505fecf
osintsevse <osintsevse@gmail.com>
2021-04-02 22:39:46 +0300

    Configured beans for all Repositories

--- view/src/main/java/com/thirdwheel/carbase/view/configuration/RepositoriesConfig.java
+++ view/src/main/java/com/thirdwheel/carbase/view/configuration/RepositoriesConfig.java
@@ -1,6 +1,6 @@
 package com.thirdwheel.carbase.view.configuration;
 
-import com.thirdwheel.carbase.dao.models.Vendor;
+import com.thirdwheel.carbase.dao.models.*;
 import com.thirdwheel.carbase.dao.repositories.GeneralRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +10,33 @@ public class RepositoriesConfig {
     @Bean
     public GeneralRepository<Vendor> vendorGeneralRepository(){
         return new GeneralRepository<>(Vendor.class);
+    }
+    @Bean
+    public GeneralRepository<Suspension> suspensionGeneralRepository(){
+        return new GeneralRepository<>(Suspension.class);
+    }
+    @Bean
+    public GeneralRepository<Modification> modificationGeneralRepository(){
+        return new GeneralRepository<>(Modification.class);
+    }
+    @Bean
+    public GeneralRepository<Model> modelGeneralRepository(){
+        return new GeneralRepository<>(Model.class);
+    }
+    @Bean
+    public GeneralRepository<Generation> generationGeneralRepository(){
+        return new GeneralRepository<>(Generation.class);
+    }
+    @Bean
+    public GeneralRepository<EngineModification> engineModificationGeneralRepository(){
+        return new GeneralRepository<>(EngineModification.class);
+    }
+    @Bean
+    public GeneralRepository<Engine> engineGeneralRepository(){
+        return new GeneralRepository<>(Engine.class);
+    }
+    @Bean
+    public GeneralRepository<Chassis> chassisGeneralRepository(){
+        return new GeneralRepository<>(Chassis.class);
     }
 }
