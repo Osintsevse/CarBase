3c5dc14f908c35903e08784952122f33fd69d27b
Osintsevse <osintsevse@gmail.com>
2021-07-07 18:48:23 +0300

    Added vendorId validation

--- view/src/main/java/com/thirdwheel/carbase/view/controller/ModelController.java
+++ view/src/main/java/com/thirdwheel/carbase/view/controller/ModelController.java
@@ -7,23 +7,26 @@ import com.thirdwheel.carbase.view.model.UniversalEntityForResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
+@Validated
 @Controller
 @AllArgsConstructor
 public class ModelController {
     private final ModelService vendorService;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/vendors/{vendorID}/models")
+    @RequestMapping(method = RequestMethod.GET, path = "/vendors/{vendorId}/models")
     public ResponseEntity<List<UniversalEntityForResponse>> getVendors(
-            @PathVariable(value = "vendorID") String vendorID,
+            @PathVariable(value = "vendorId") @Pattern(regexp = "[0-9]+") String vendorId,
             @RequestParam(value = "nameBeginning", required = false) String nameBeginning) {
-        List<Model> models = vendorService.getByVendor(Integer.parseInt(vendorID), nameBeginning);
+        List<Model> models = vendorService.getByVendor(Integer.parseInt(vendorId), nameBeginning);
         return ResponseEntity.ok(new EntitiesListResponse(models).getEntities());
     }
 }
