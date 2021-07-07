0fb9e9fcefb14b9b08dcf498274b9b53d81fcdaa
osintsevse <osintsevse@gmail.com>
2021-07-07 12:56:20 +0300

    request body replaced with request param and url param

--- view/src/main/java/com/thirdwheel/carbase/view/controller/ModelController.java
+++ view/src/main/java/com/thirdwheel/carbase/view/controller/ModelController.java
@@ -3,14 +3,11 @@ package com.thirdwheel.carbase.view.controller;
 import com.thirdwheel.carbase.dao.models.Model;
 import com.thirdwheel.carbase.service.ModelService;
 import com.thirdwheel.carbase.view.model.EntitiesListResponse;
-import com.thirdwheel.carbase.view.model.ModelRequest;
 import com.thirdwheel.carbase.view.model.UniversalEntityForResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,10 +16,10 @@ import java.util.List;
 public class ModelController {
     private final ModelService vendorService;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/models")
+    @RequestMapping(method = RequestMethod.GET, path = "/vendors/{vendorID}/models")
     public ResponseEntity<List<UniversalEntityForResponse>> getVendors
-            (@RequestBody ModelRequest modelRequest) {
-        List<Model> models = vendorService.getModels(modelRequest.getVendorId(), modelRequest.getNameBeginning());
+            (@PathVariable(value="vendorID") String vendorID,@RequestParam(value="nameBeginning",required = false) String nameBeginning ) {
+        List<Model> models = vendorService.getModels(Integer.parseInt(vendorID), nameBeginning);
         return ResponseEntity.ok(new EntitiesListResponse(models).getEntities());
     }
 }
--- view/src/main/java/com/thirdwheel/carbase/view/controller/VendorController.java
+++ view/src/main/java/com/thirdwheel/carbase/view/controller/VendorController.java
@@ -4,13 +4,12 @@ import com.thirdwheel.carbase.dao.models.Vendor;
 import com.thirdwheel.carbase.service.GeneralService;
 import com.thirdwheel.carbase.view.model.EntitiesListResponse;
 import com.thirdwheel.carbase.view.model.UniversalEntityForResponse;
-import com.thirdwheel.carbase.view.model.VendorRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,12 +20,12 @@ public class VendorController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/vendors")
     public ResponseEntity<List<UniversalEntityForResponse>> getVendors
-            (@RequestBody(required = false) VendorRequest vendorRequest) {
+            (@RequestParam(value="nameBeginning",required = false) String nameBeginning) {
         List<Vendor> vendors;
-        if (vendorRequest == null) {
+        if (nameBeginning == null) {
             vendors = vendorService.getAll();
         } else {
-            vendors = vendorService.getByNameBeginnig(vendorRequest.getNameBeginning());
+            vendors = vendorService.getByNameBeginnig(nameBeginning);
         }
         return ResponseEntity.ok(new EntitiesListResponse(vendors).getEntities());
     }
