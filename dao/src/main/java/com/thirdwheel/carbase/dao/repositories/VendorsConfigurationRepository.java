5e72dc245ce61819acf9293e7cafe58ce2406ea3
Osintsevse <osintsevse@gmail.com>
2021-07-08 16:27:58 +0300

    added VendorsConfigurationRepository

---
+++ dao/src/main/java/com/thirdwheel/carbase/dao/repositories/VendorsConfigurationRepository.java
@@ -0,0 +1,38 @@
+package com.thirdwheel.carbase.dao.repositories;
+
+import com.thirdwheel.carbase.dao.models.Vendor;
+import com.thirdwheel.carbase.dao.models.VendorsConfiguration;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Service
+public class VendorsConfigurationRepository extends GeneralEntityRepository<VendorsConfiguration> {
+    public VendorsConfigurationRepository() {
+        super(VendorsConfiguration.class);
+    }
+
+    public VendorsConfiguration getByVendor(Integer vendorId) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<VendorsConfiguration> cq = cb.createQuery(tClass);
+        Root<VendorsConfiguration> modelRoot = cq.from(tClass);
+        CriteriaQuery<VendorsConfiguration> modelsByVendorId = cq.where(cb.equal(modelRoot.get("vendor"), vendorId));
+        TypedQuery<VendorsConfiguration> query = entityManager.createQuery(modelsByVendorId);
+        List<VendorsConfiguration> resultList = query.getResultList();
+        if (resultList.size() > 0) {
+            return resultList.get(0);
+        } else return null;
+    }
+
+    public VendorsConfiguration getByVendor(Vendor vendor) {
+        int vendorId = vendor.getId();
+        if (vendorId != 0) {
+            return this.getByVendor(vendorId);
+        }
+        return null;
+    }
+}
