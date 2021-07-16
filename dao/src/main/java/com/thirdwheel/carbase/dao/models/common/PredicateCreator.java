e7247abaf6e7594628e567ec41ba2e3c14519ea4
Osintsevse <osintsevse@gmail.com>
2021-07-17 00:02:55 +0300

    Added year validation in PredicateCreator

--- dao/src/main/java/com/thirdwheel/carbase/dao/models/common/PredicateCreator.java
+++ dao/src/main/java/com/thirdwheel/carbase/dao/models/common/PredicateCreator.java
@@ -3,19 +3,27 @@ package com.thirdwheel.carbase.dao.models.common;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
+import javax.validation.ConstraintViolationException;
 import java.time.LocalDate;
+import java.util.regex.Pattern;
 
 public class PredicateCreator {
-    public static Predicate yearBetweenStartAndEnd(Path<LocalDate> start, Path<LocalDate> end, String year,
+    public static Predicate yearBetweenStartAndEnd(Path<LocalDate> manufacturingStartDate,
+                                                   Path<LocalDate> manufacturingEndDate,
+                                                   String year,
                                                    CriteriaBuilder cb) {
+        Pattern pattern = Pattern.compile("[1-2]{4}");
+        if (!pattern.matcher(year).matches()){
+            throw new ConstraintViolationException("Year must match \""+pattern.pattern()+"\"",null);
+        }
         LocalDate beginningOfYear = LocalDate.parse(year + "-01-01");
         LocalDate endOfYear = LocalDate.parse(year + "-12-31");
         Predicate startPredicate = cb.or(
-                cb.lessThanOrEqualTo(start, endOfYear),
-                cb.isNull(start));
+                cb.lessThanOrEqualTo(manufacturingStartDate, endOfYear),
+                cb.isNull(manufacturingStartDate));
         Predicate endPredicate = cb.or(
-                cb.greaterThanOrEqualTo(end, beginningOfYear),
-                cb.isNull(end));
+                cb.greaterThanOrEqualTo(manufacturingEndDate, beginningOfYear),
+                cb.isNull(manufacturingEndDate));
         return cb.and(startPredicate, endPredicate);
     }
 
@@ -27,7 +35,7 @@ public class PredicateCreator {
         return cb.equal(cb.upper(stringField), stringValue.toUpperCase());
     }
 
-    public static Predicate stringIsLike(Path<String> stringField, String stringValue, CriteriaBuilder cb) {
-        return cb.like(cb.upper(stringField), stringValue.toUpperCase() + "%");
+    public static Predicate stringStartsWith(Path<String> stringField, String stringBeginning, CriteriaBuilder cb) {
+        return cb.like(cb.upper(stringField), stringBeginning.toUpperCase() + "%");
     }
 }
